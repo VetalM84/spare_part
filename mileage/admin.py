ff8c5058f63a83e855d1193efbce3a93d5b6c24e
Виталик <vetalm@i.ua>
2021-10-10 22:26:50 +0300

    Initial commit - models - admin settings

---
+++ mileage/admin.py
@@ -0,0 +1,30 @@
+from django.contrib import admin
+
+from .models import SparePart, Car, Mileage
+
+
+class SparePartAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'brand', 'number')
+    list_display_links = ('id', 'name', 'brand', 'number')
+    search_fields = ('name', 'brand', 'number')
+    # list_editable = ('is_published',)
+    list_filter = ('name', 'brand')
+
+
+class CarAdmin(admin.ModelAdmin):
+    list_display = ('id', 'brand', 'model_name', 'model_variant')
+    list_display_links = ('id', 'brand', 'model_name')
+    search_fields = ('brand', 'model_name', 'model_variant')
+    list_filter = ('brand', 'model_name')
+
+
+class MileageAdmin(admin.ModelAdmin):
+    list_display = ('id', 'spare_part', 'car', 'mileage')
+    list_display_links = ('id', 'spare_part', 'car')
+    search_fields = ('spare_part', 'car')
+    list_filter = ('spare_part', 'car')
+
+
+admin.site.register(SparePart, SparePartAdmin)
+admin.site.register(Car, CarAdmin)
+admin.site.register(Mileage, MileageAdmin)
---
+++ mileage/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+
+
+class Car(models.Model):
+    brand = models.CharField(max_length=40, verbose_name="Марка")
+    model_name = models.CharField(max_length=60, verbose_name="Модель")
+    model_variant = models.CharField(max_length=100, verbose_name="Модификация")
+    age = models.SmallIntegerField(verbose_name="Год выпуска")
+
+    def __str__(self):
+        return ' '.join([self.brand, self.model_name, self.model_variant])
+
+    class Meta:
+        verbose_name = 'Автомобиль'
+        verbose_name_plural = 'Автомобили'
+        ordering = ['brand']
+
+
+class SparePart(models.Model):
+    name = models.CharField(max_length=255, verbose_name='Название')
+    brand = models.CharField(max_length=255, verbose_name="Производитель")
+    number = models.CharField(max_length=30, unique=True, verbose_name="Номер")
+
+    def __str__(self):
+        return ' '.join([self.name, self.brand, self.number])
+
+    class Meta:
+        verbose_name = 'Запчасть'
+        verbose_name_plural = 'Запчасти'
+        ordering = ['name']
+
+
+class Mileage(models.Model):
+    spare_part = models.ForeignKey('SparePart', on_delete=models.PROTECT, verbose_name="Запчасть")
+    car = models.ForeignKey('Car', on_delete=models.PROTECT, verbose_name="Автомобиль")
+    mileage = models.SmallIntegerField(verbose_name="Пробег, тыс.км")
+
+    def __str__(self):
+        return ' '.join([self.spare_part.name, "на", self.car.brand, self.car.model_name])
+
+    class Meta:
+        verbose_name = 'Пробег'
+        verbose_name_plural = 'Пробег'
