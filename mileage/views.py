971ac2c97b8d99ac6b00ae000d712b92cb1f140a
Виталик <vetalm@i.ua>
2021-10-12 11:45:14 +0300

    - similar spare parts list

--- mileage/views.py
+++ mileage/views.py
@@ -31,12 +31,18 @@ def get_car_spare_parts(request, car_id):
 
 
 def get_spare_parts_mileages(request, car_id, spare_part_id):
-    """ получаем список всех записей о пробеге для конкретной запчасти а также конкретной марки и модели авто """
+    """ получаем список всех записей о пробеге для конкретной запчасти на конкретной марке и модели авто """
     spare_parts = Mileage.objects.filter(car_id=car_id, spare_part_id=spare_part_id).order_by('-mileage')
-    car = Car.objects.get(id=spare_part_id)
     max_mileage = spare_parts.aggregate(Max('mileage'))
     min_mileage = spare_parts.aggregate(Min('mileage'))
     avg_mileage = spare_parts.aggregate(Avg('mileage'))
+    records_count = spare_parts.count()
+
+    car = Car.objects.get(id=spare_part_id)
+    # список похожих запчастей по имени запчасти
+    current_spare_part_name = SparePart.objects.get(id=spare_part_id).name
+    similar_spare_parts = SparePart.objects.filter(name__contains=current_spare_part_name)
+
     context = {
         'spare_parts': spare_parts,
         'title': 'Список пробегов запчасти для',
@@ -47,5 +53,7 @@ def get_spare_parts_mileages(request, car_id, spare_part_id):
         'min_mileage': min_mileage['mileage__min'],
         'max_mileage': max_mileage['mileage__max'],
         'avg_mileage': avg_mileage['mileage__avg'],
+        'records_count': records_count,
+        'similar_spare_parts': similar_spare_parts,
     }
     return render(request, 'mileage/spare_part.html', context)
