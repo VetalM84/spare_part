7b0ddeefd7acb554edf12810f90daf2492427937
Виталик <vetalm@i.ua>
2021-10-14 13:37:05 +0300

    django-heroku 2

--- spare_part/settings.py
+++ spare_part/settings.py
@@ -127,5 +127,3 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-# Activate Django-Heroku.
-django_heroku.settings(locals())
