8f34c79c8fa0a88bc8a85009b5a5c1a0e91f9aa2
Виталик <vetalm@i.ua>
2021-10-15 13:38:41 +0300

    get_absolute_url for Car

--- mileage/models.py
+++ mileage/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.urls import reverse_lazy
 
 
 class Car(models.Model):
@@ -12,6 +13,9 @@ class Car(models.Model):
 
     def __str__(self):
         return ' '.join([self.brand, self.model_name, self.model_variant])
+
+    def get_absolute_url(self):
+        return reverse_lazy('car_spare_parts', kwargs={'car_id': self.pk})
 
     class Meta:
         verbose_name = 'Автомобиль'
