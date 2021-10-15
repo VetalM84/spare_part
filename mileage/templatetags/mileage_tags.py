770cfbe4e4440f6954e747577125802770e690ca
Виталик <vetalm@i.ua>
2021-10-15 13:53:39 +0300

    get_object_or_404 in templatetags

--- mileage/templatetags/mileage_tags.py
+++ mileage/templatetags/mileage_tags.py
@@ -1,4 +1,5 @@
 from django import template
+from django.shortcuts import get_object_or_404
 
 from mileage.models import User, Car
 
@@ -7,16 +8,13 @@ register = template.Library()
 
 @register.simple_tag
 def get_user_info(user_id):
-    user = User.objects.get(id=user_id)
+    # user = User.objects.get(id=user_id)
+    user = get_object_or_404(User, pk=user_id)
     return user
 
 
 @register.simple_tag
 def get_car_info(car_id):
-    car = Car.objects.get(id=car_id)
+    # car = Car.objects.get(id=car_id)
+    car = get_object_or_404(Car, pk=car_id)
     return car
-
-# @register.inclusion_tag('news/list_categories.html')
-# def show_categories(arg1='Hello', arg2='world'):
-#     categories = Category.objects.all()
-#     return {"categories": categories, "arg1": arg1, "arg2": arg2}
