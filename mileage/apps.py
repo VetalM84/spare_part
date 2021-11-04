2bd699976697ae611a3dfaab963167993303a6ae
Виталик <vetalm@i.ua>
2021-11-04 23:19:51 +0200

    app verbose_name in django admin

--- mileage/apps.py
+++ mileage/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class MileageConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'mileage'
+    verbose_name = "Отзывы о запчастях"
