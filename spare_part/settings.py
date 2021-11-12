e2ce85a8d1458d317c19eb1acabeb8fbc8bf4b56
Виталик <vetalm@i.ua>
2021-11-12 10:39:53 +0200

    moved to postgresql

--- spare_part/settings.py
+++ spare_part/settings.py
@@ -75,13 +75,22 @@ WSGI_APPLICATION = 'spare_part.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'spare_part_db',
+        'USER': 'sp_django',
+        'PASSWORD': '11111111',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
